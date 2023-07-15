28830d4adbaf9f8b6647a8d10c341b27dbcf31f5
Mackenzie Lach <kenzie@Mackenzies-MacBook-Pro.local>
2023-07-14 20:04:45 -0700

    added class function to convert cm (x,y) coords to m

--- nrm-artist/design_class.py
+++ nrm-artist/design_class.py
@@ -19,7 +19,8 @@ class design:
     def __init__(self, nholes, hrad):
         self.nholes = nholes
         self.hrad = hrad
-        self.xy_coords = np.empty([nholes, 2])
+        self.xy_coords_cm = np.empty([nholes, 2])
+        self.xy_coords_m = np.empty([nholes, 2])
         self.uv_coords = np.empty([x_choose_y(self.nholes, 2), 2])
         
     def make_uv_coords(self, uv_coords):
@@ -40,4 +41,13 @@ class design:
     def get_uvs(self):
         self.uv_coords = self.make_uv_coords(self.uv_coords)
         return self.uv_coords
+    
+    def make_xy_coords_m(self, xy_coords_cm):
+        xy_coords_m = xy_coords_cm / 100
+        self.xy_coords_m = xy_coords_m
+        return self.xy_coords_m
+    
+    def get_xy_m(self):
+        self.xy_coords_m = self.make_xy_coords_m(self.xy_coords_cm)
+        return self.xy_coords_m
                     
