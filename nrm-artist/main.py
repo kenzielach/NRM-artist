a5930892197911ae7582122746837be4203b0ec0
Mackenzie Lach <kenzie@dhcp-v034-134.mobile.uci.edu>
2023-10-25 15:28:32 -0700

    making sure everything is up-to-date

--- nrm-artist/main.py
+++ nrm-artist/main.py
@@ -9,9 +9,20 @@ hrad = float(input())
 check_valid_hrad(hrad)
 
 print('Thanks! Generating mask design...')
-mask_design = make_design(nholes, hrad)
-print('Would you like to save your mask as an .npy (y/n)?')
-ans = str(input())
-if ans == 'y':
+
+if nholes > 6:
+    mask_design, vcoords = make_design(6, hrad, return_vcoords=True)
+    diff = nholes - 6
+    mask_design = add_to_design(mask_design, diff, vcoords)    
     save_design(mask_design)
-print('Done! Enjoy your mask :)')
+    print('Done! Enjoy your mask :)')
+
+else:
+    mask_design = make_design(nholes, hrad)
+    #print('Would you like to save your mask as an .npy (y/n)?')
+    #ans = str(input())
+    #if ans == 'y':
+    print("Yay! Mask design is non-redundant. Plotting design...")
+    #plot_design(mask_design, aperture)
+    save_design(mask_design)
+    print('Done! Enjoy your mask :)')
