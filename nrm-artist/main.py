1c761fa15d57dba7bab3ca792c0cf8fcdf80ebfa
Mackenzie Lach <kenzie@Mackenzies-MBP.lan>
2023-10-26 17:03:05 -0700

    beginning of optimize branch

--- nrm-artist/main.py
+++ nrm-artist/main.py
@@ -8,21 +8,26 @@ print('Please enter the hole radius in meters:')
 hrad = float(input())
 check_valid_hrad(hrad)
 
-print('Thanks! Generating mask design...')
+print('Generating mask design...')
 
 if nholes > 6:
-    mask_design, vcoords = make_design(6, hrad, return_vcoords=True)
-    diff = nholes - 6
-    mask_design = add_to_design(mask_design, diff, vcoords)    
-    save_design(mask_design)
-    print('Done! Enjoy your mask :)')
+    count = 1
+    while 1:
+        mask_design, vcoords = make_design(6, hrad, return_vcoords=True)
+        diff = nholes - 6
+        mask_design = add_to_design(mask_design, diff, vcoords) 
+        print("iteration # " + str(count))   
+        if mask_design.nholes == nholes:
+            break
+        count += 1
+    print('Would you like to save your mask as an .npy (y/n)?')
+    ans = str(input())
+    if ans == 'y':
+        save_design(mask_design)
 
 else:
     mask_design = make_design(nholes, hrad)
-    #print('Would you like to save your mask as an .npy (y/n)?')
-    #ans = str(input())
-    #if ans == 'y':
-    print("Yay! Mask design is non-redundant. Plotting design...")
-    #plot_design(mask_design, aperture)
-    save_design(mask_design)
-    print('Done! Enjoy your mask :)')
+    print('Would you like to save your mask as an .npy (y/n)?')
+    ans = str(input())
+    if ans == 'y':
+        save_design(mask_design)
